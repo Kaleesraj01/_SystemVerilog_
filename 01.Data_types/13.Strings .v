06b3d5fc246bbe665705960bebb5cbe13bc5589c
Kaleesraj M <kalishraj01@gmail.com>
2025-11-17 10:29:22 +0530

    Update 13.Strings .v

--- 01.Data_types/13.Strings .v
+++ 01.Data_types/13.Strings .v
@@ -31,3 +31,38 @@ module string_ex ;
 # KERNEL:  substr = RAJ  
 # KERNEL: Get index = o 
 # KERNEL: Put value = hello woald  
+
+
+
+module string_basic_methods;
+  string s1="Hi Everyone";//11
+  string s2="He Everyone";//11
+  string s3;
+  
+  initial begin
+    $display("s1=%0d",s1.len());
+    s1.putc(2,"char");
+    $display("s1=%0s",s1);
+    // s1.putc(2,"char")     //s1=Hiceveryone  //oneletteronly
+
+    $display("s1=%0s=%0d", s1.getc(2), s1.getc(2));
+    
+    s1="ENGINEERING";
+    $display("s1=%0s",s1.tolower());
+    $display("s1=%0s",s1.toupper());
+    
+   // s2="ENGINEERING";
+    s2="Engineering";
+    $display("s1=%0s s2=%0s difference=%0d",s1,s2,s1.compare(s2));
+    //(s1-s2)case sensitive(0=equ,neg=s1<s2,pos=s1>s2)
+    $display("s1=%0s s2=%0s difference=%0d",s1,s2,s1.icompare(s2)); //case insensitive
+  end
+endmodule 
+OUTPUT 
+s1=11
+s1=HicEveryone
+s1=c=99
+s1=engineering
+s1=ENGINEERING
+s1=ENGINEERING s2=Engineering difference=-32
+s1=ENGINEERING s2=Engineering difference=0
