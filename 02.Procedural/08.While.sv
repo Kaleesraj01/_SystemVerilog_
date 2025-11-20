7136cdfdcf0edd26993d82fbc60487d31bdf00c8
Kaleesraj M <kalishraj01@gmail.com>
2025-11-20 18:40:46 +0530

    Create 08.While.sv

---
+++ 02.Procedural/08.While.sv
@@ -0,0 +1,27 @@
+module while_loop ;
+  int cnt =6;
+  
+  initial begin 
+    while (cnt > 5 )begin 
+      cnt++ ;
+    $display ("count = %0d , cnt );
+              if (cnt == 15) break ;
+              end 
+              end 
+              
+              endmodule 
+              
+    OUTPUT 
+# count = 7
+# count = 8
+# count = 9
+# count = 10
+# count = 11
+# count = 12
+# count = 13
+# count = 14
+# count = 15
+    
+
+    
+   
