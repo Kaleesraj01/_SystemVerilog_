8cea062b52f7d5011f6a33c1d49104b7e17962ad
Kaleesraj M <kalishraj01@gmail.com>
2025-11-24 11:50:16 +0530

    Update 12.Enum.sv

--- 01.Data_types/12.Enum.sv
+++ 01.Data_types/12.Enum.sv
@@ -19,3 +19,29 @@ endmodule
 
 // OUTPUT 
 //  Transport 1 = AEROPLANE 
+
+
+
+
+module enum_ex;
+  enum {CAR , BIKE , TRAIN , AEROPLANE , BOAT , CYCLE } transport_e , trans , raj ;
+//   transport_e trans_1 , trans_2 , trans_3 , trans_4 ;
+  initial begin
+
+    transport_e = CAR ;
+    trans = BOAT;
+    raj =  AEROPLANE;
+    
+    
+    $display("Transport 1 = %s", transport_e.name());
+    $display("Transport 2 = %s", trans.name());
+    $display("Transport 2 = %s", raj.name());
+
+ end
+endmodule
+
+
+OUTPUT 
+Transport 1 = CAR
+Transport 2 = BOAT
+Transport 2 = AEROPLANE
