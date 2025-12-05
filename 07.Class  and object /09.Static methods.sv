5335ff042d36d54e8db645546025ab8f1a462cfe
Kaleesraj M <kalishraj01@gmail.com>
2025-12-05 11:50:52 +0530

    Update 09.Static methods.sv

--- 07.Class  and object /09.Static methods.sv
+++ 07.Class  and object /09.Static methods.sv
@@ -33,3 +33,51 @@ OUTPUT
 # TOTAL pkts = 2
 # TOTAL pkts = 3
 # TOTAL pkts = 4
+
+////////////////////////////////////////////////////////////////////////////////
+
+class transaction;
+  static int s_id;
+  int id;
+  
+  static function void incr_s_id(); // Static function
+    s_id++;
+    //id++; // illegal access
+  endfunction
+  
+  function void incr_id(); // Non-static function
+    s_id++;
+    id++;
+  endfunction
+  
+endclass
+
+module class_example;
+  transaction tr[5], tr_new;
+  initial begin
+    foreach (tr[i]) begin 
+      tr[i] = new();
+      tr[i].incr_s_id();
+      $display("On calling incr_s_id: Value of s_id = %0h, id = %0h", tr[i].s_id, tr[i].id);
+    end
+    tr[0].s_id = 0; // s_id value reset to 0
+    foreach (tr[i]) begin 
+      tr[i] = new();
+      tr[i].incr_id();
+      $display("On calling incr_id: Value of s_id = %0h, id = %0h", tr[i].s_id, tr[i].id);
+    end
+  end
+endmodule
+
+OUTPUT
+ On calling incr_s_id: Value of s_id = 1, id = 0
+# On calling incr_s_id: Value of s_id = 2, id = 0
+# On calling incr_s_id: Value of s_id = 3, id = 0
+# On calling incr_s_id: Value of s_id = 4, id = 0
+# On calling incr_s_id: Value of s_id = 5, id = 0
+# On calling incr_id: Value of s_id = 1, id = 1
+# On calling incr_id: Value of s_id = 2, id = 1
+# On calling incr_id: Value of s_id = 3, id = 1
+# On calling incr_id: Value of s_id = 4, id = 1
+# On calling incr_id: Value of s_id = 5, id = 1
+# exit
