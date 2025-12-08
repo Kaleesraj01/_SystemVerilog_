a0d098483214ab1dc4426ef34de4bf0944f56b50
Kaleesraj M <kalishraj01@gmail.com>
2025-12-08 10:45:57 +0530

    Update 1.Without  Virtual method.sv

--- 09.Virtual concept/1.Without  Virtual method.sv
+++ 09.Virtual concept/1.Without  Virtual method.sv
@@ -1,17 +1,17 @@
 // Code your testbench here
 // or browse Examples
 class packet ;
-  string 
-  m= "raj";
+  int
+  m= 264;
   
-  function string show();
+  function int show();
     show = m;
   endfunction 
 endclass
 
 class child extends packet;
   int i = 87;
-  function int show();
+  function int show(); // to override returntype must be same 
     show = i ;
   endfunction 
 endclass 
@@ -22,12 +22,15 @@ module tb;
     packet a1 = c1 ;// parent handles only the packet variable and methods in the child 
     
     $display ("m=%0d ",a1.m);
-    $display ("show =%0s",a1.show); // even though fn overrides it takes only baseclass 
+    $display ("show =%0d",a1.show); // even though fn overrides it takes only baseclass 
     
   end
 endmodule
+     
+  
+  
     OUTPUT 
-# m=raj 
-# show =raj
+# m=264
+# show =264
   
   
