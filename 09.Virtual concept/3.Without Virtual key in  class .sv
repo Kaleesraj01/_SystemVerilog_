d0c29bab6c29dda29ab3975571f7e46ca1830f4b
Kaleesraj M <kalishraj01@gmail.com>
2025-12-08 12:46:29 +0530

    Update 3.Without Virtual key in  class .sv

--- 09.Virtual concept/3.Without Virtual key in  class .sv
+++ 09.Virtual concept/3.Without Virtual key in  class .sv
@@ -21,3 +21,28 @@ OUTPUT
 Base Value of data = 5 and id = 1
 
 
+virtual class parent_trans;   // virtual class cant be instaintiated can only be derived !!!! 
+  bit [31:0] data;
+  int id;
+  
+  function void display();
+    $display("Base: Value of data = %0h and id = %0h", data, id);
+  endfunction
+endclass
+
+module class_example;
+  initial begin
+    parent_trans p_tr;
+    p_tr = new();
+    
+    p_tr.data = 5;
+    p_tr.id = 1;
+    p_tr.display();
+  end
+endmodule
+output :
+class_example, "p_tr = new();"
+  Instantiation of the object 'p_tr' can not be done because its type 
+  'parent_trans' is an abstract base class.
+  Perhaps there is a derived class that should be used.
+
