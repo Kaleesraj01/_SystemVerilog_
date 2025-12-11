784b835988d25dafb3ce1e059a6fe08c1bc6414e
Kaleesraj M <kalishraj01@gmail.com>
2025-12-11 15:32:51 +0530

    Update 4.instance handle error.sv

--- 13.Constant and Typedef/4.instance handle error.sv
+++ 13.Constant and Typedef/4.instance handle error.sv
@@ -15,7 +15,7 @@ module tb ;
   parent p1;
   initial begin 
     p2=new();
-    p1=new();
+    p1=new();  // cant instance after the constructor
     p1.value = 234344;
     $display("p1val =%0d", p1.value );
     
@@ -29,3 +29,27 @@ testbench.sv, 19
   Variable 'value' declared as 'const' cannot be used in this context
   Source info: p1.value = 234344;
 
+==============================================================================================================================================================
+
+class parent ;
+  int addr ;
+  const int value;
+  
+  function void raj();  // illegal can be done by only constructor
+    addr = 243;
+    value = 44685;  // Instance constant are dynamic and created at runtime only 
+    $display ("addr=%0d ,value =%0d  ", addr , value );
+  endfunction 
+  
+endclass 
+
+module tb ;
+  parent p2 ;
+  
+  initial begin 
+    p2=new();
+    p2.raj;
+   
+  end 
+endmodule 
+    
