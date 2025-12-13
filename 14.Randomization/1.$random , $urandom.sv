b2806c6fbaedfef99312557abbd544ef6d133c6f
Kaleesraj M <kalishraj01@gmail.com>
2025-12-13 23:58:26 +0530

    Update 1.$random , $urandom.sv

--- 14.Randomization/1.$random , $urandom.sv
+++ 14.Randomization/1.$random , $urandom.sv
@@ -28,7 +28,7 @@ module mod;
   initial begin 
     repeat (7) begin
     data = $random ;
-    bitt = $urandom_range;
+     bitt = $urandom_range(5,8);
       
       $display ("data=%0d,bitt=%0d",data,bitt);
     end
@@ -36,3 +36,14 @@ module mod;
     
   end
 endmodule
+
+
+OUTPUT  
+# data=303379748,bitt=8
+# data=-1064739199,bitt=7
+# data=-2071669239,bitt=7
+# data=-1309649309,bitt=6
+# data=112818957,bitt=5
+# data=1189058957,bitt=7
+# data=-1295874971,bitt=5
+
