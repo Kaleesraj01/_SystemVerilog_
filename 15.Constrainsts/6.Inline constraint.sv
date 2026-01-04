7198b540ba97ee2beb72e8976abae3a1d7bdefea
Kaleesraj M <kalishraj01@gmail.com>
2026-01-04 23:54:31 +0530

    Create 6.Inline constraint.sv

---
+++ 15.Constrainsts/6.Inline constraint.sv
@@ -0,0 +1,25 @@
+// Code your testbench here
+// or browse Examples
+class cons;
+  rand int data;
+
+  constraint c1 {
+    data inside {[0:100]};
+  }
+endclass
+
+module tb;
+  cons i;
+
+  initial begin
+    i = new();
+
+    // Normal randomization
+    i.randomize();
+    $display("Normal data = %0d", i.data);
+
+    // Inline constraint 
+    i.randomize() with { data inside {[50:60]}; };
+    $display("Inline constrained data = %0d", i.data);
+  end
+endmodule
