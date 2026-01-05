e48c46632e150bd7aa40e5d8511c3b878799a7c5
Kaleesraj M <kalishraj01@gmail.com>
2026-01-05 15:42:35 +0530

    Update 9.Patterngeneration.sv

--- 15.Constrainsts/9.Patterngeneration.sv
+++ 15.Constrainsts/9.Patterngeneration.sv
@@ -62,5 +62,51 @@ module patterngen ;
        
 endmodule
     
-   KERNEL: the required pattern is :=  0 1 0 1 0 1 0 1 0 1 0 1 0 1 
+   KERNEL: the required pattern is :=  0 1 0 1 0 1 0 1 0 1 0 1 0 1   
+
+
+
+// Code your testbench here
+// or browse Examples
+//----------------------
+// Power-of-2 generator
+//----------------------
+class power2;
+  randc int unsigned value;
+
+  // Constraint: value must be power of 2
+  constraint power2 {
+    value > 0;                       
+    (value & (value - 1)) == 0;      // power-of-2 
+    value inside {[1:1024]};       
+  }
+endclass
+
+
+
+module tb_pow2;
+  initial begin
+    power2 p;
+    p = new();
+
+    repeat (8) begin
+      if (p.randomize())
+        $display("Power of 2 value = %0d (binary = %b)", p.value, p.value);
+      else
+        $display("Randomization FAILED");
+      #1;
+    end
+
+    $finish;
+  end
+endmodule
+OUTPUT 
+ Power of 2 value = 256 (binary = 00000000000000000000000100000000)
+# Power of 2 value = 128 (binary = 00000000000000000000000010000000)
+# Power of 2 value = 1 (binary = 00000000000000000000000000000001)
+# Power of 2 value = 16 (binary = 00000000000000000000000000010000)
+# Power of 2 value = 4 (binary = 00000000000000000000000000000100)
+# Power of 2 value = 64 (binary = 00000000000000000000000001000000)
+# Power of 2 value = 32 (binary = 00000000000000000000000000100000)
+# Power of 2 value = 2 (binary = 00000000000000000000000000000010)
    
