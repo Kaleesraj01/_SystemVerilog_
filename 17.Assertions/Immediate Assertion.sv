2ecde654818b750af0d02d7aea8bb910104e08e6
Kaleesraj M <kalishraj01@gmail.com>
2026-02-23 23:54:25 +0530

    Create Immediate Assertion.sv

---
+++ 17.Assertions/Immediate Assertion.sv
@@ -0,0 +1,32 @@
+module immediate_assert_example;
+
+  logic clk;
+  logic a, b;
+  logic y;
+
+  
+  assign y = a & b;
+
+  // Clock generation
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  
+  initial begin
+    a = 0; b = 0;
+    #10 a = 1;
+    #10 b = 1;
+    #10 a = 0;
+    #10 b = 0;
+    #10 $finish;
+  end
+
+  // Immediate Assertion
+  always @(posedge clk) begin
+    assert (y == (a & b))
+      else $error("Assertion Failed at time %0t: y != a & b", $time);
+  end
+
+endmodule
